876d865ddbf51b08803beb059b83e37f77c3a5ad
Lucas Vendramini <lvfx23@gmail.com>
2026-02-23 19:19:16 -0300

    feat: add index page, bootstrap plugin, and update nuxt config for staging

---
+++ app/pages/index.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="index-page">
+        <div class="container d-flex justify-content-center">
+            <div class="card mt-3 border-0 text-center">
+                <div class="card-body">
+                    <img
+                        src="https://avatars.githubusercontent.com/u/146987511"
+                        class="rounded-circle"
+                        width="256"
+                        height="256"
+                        alt="lucas-avatar"
+                    />
+                    <h1 class="card-title text-center mt-4">
+                        Lucas Vendramini
+                    </h1>
+                    <h3 class="card-subtitle text-center">
+                        Desenvolvedor Fullstack
+                    </h3>
+
+                    <p class="mt-4">Em breve novo site. Aguarde... 🚜</p>
+                    <!-- <a href="#">Clique para conhecer</a>
+
+                    <div class="mt-4">
+                        <button type="button" class="btn btn-primary">
+                            Linkedin
+                        </button>
+                    </div> -->
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<style lang="scss">
+body {
+    height: 100vh;
+    overflow: hidden;
+}
+
+.index-page {
+    height: 100vh;
+
+    * {
+        color: white;
+    }
+
+    .card {
+        background: transparent;
+        max-width: 480px;
+        min-width: 320px;
+    }
+}
+</style>
